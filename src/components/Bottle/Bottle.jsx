fe6f25a9d7009edd81e01491dcfc23e3b2a4943a
emoncse2020 <emon.cse.2020@gmail.com>
2025-05-16 19:47:25 +0600

    showed all bottles

--- src/components/Bottle/Bottle.jsx
+++ src/components/Bottle/Bottle.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import "./Bottle.css";
 
-const Bottle = () => {
-  return <div></div>;
+const Bottle = ({ bottle, handleAddToCart }) => {
+  //   console.log(bottle);
+  const { name, img, price } = bottle;
+  return (
+    <div className="bottle-container">
+      <h2>Botte Name : {name}</h2>
+      <img src={img} alt="" />
+      <p>Price : ${price}</p>
+      <button onClick={() => handleAddToCart(bottle)}>Purchase</button>
+    </div>
+  );
 };
 
 export default Bottle;
--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Bottle from "../Bottle/Bottle";
+import "./Bottles.css";
 
 const Bottles = () => {
+  const [bottles, setBottles] = useState([]);
+
+  useEffect(() => {
+    fetch("bottles.json")
+      .then((res) => res.json())
+      .then((data) => setBottles(data));
+  }, []);
+
+  const [cart, setCart] = useState([]);
+
+  const handleAddToCart = (bottle) => {
+    console.log("Bottle is going to be added");
+    const newCart = [...cart, bottle];
+    setCart(newCart);
+  };
+
   return (
     <div>
-      <h3>Bottles Available : {}</h3>
+      <h3>Bottles Available : {bottles.length}</h3>
+      <h4>Cart : {cart.length}</h4>
+
+      <div className="bottles-container">
+        {bottles.map((bottle) => (
+          <Bottle
+            bottle={bottle}
+            handleAddToCart={handleAddToCart}
+            key={bottle.id}
+          ></Bottle>
+        ))}
+      </div>
     </div>
   );
 };
